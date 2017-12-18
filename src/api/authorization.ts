b29505dd020782bfa26fb58fdcb34e942b21224d
Michael Neu <git@michaeln.eu>
2017-12-18 14:42:37 +0100

    let jwt token expire

--- src/api/authorization.ts
+++ src/api/authorization.ts
@@ -3,13 +3,31 @@ import { User } from "./user";
 const TOKEN_STORAGE_KEY = "tweetr_token",
       USER_STORAGE_KEY = "tweetr_user";
 
+interface TokenInformation {
+  token: string;
+  expireDate: Date;
+}
+
 export class Authorization {
   public get token() : string {
-    return localStorage.getItem(TOKEN_STORAGE_KEY) as string;
+    const json = localStorage.getItem(TOKEN_STORAGE_KEY) as string,
+          info = JSON.parse(json) as TokenInformation;
+
+    if (new Date() > info.expireDate) {
+      return "";
+    }
+
+    return info.token;
   }
 
   public set token(value: string) {
-    localStorage.setItem(TOKEN_STORAGE_KEY, value);
+    const info: TokenInformation = {
+            expireDate: new Date(Date.now() + 60 * 60 * 1000),
+            token: value,
+          },
+          json = JSON.stringify(info);
+
+    localStorage.setItem(TOKEN_STORAGE_KEY, json);
   }
 
   public get user() : User {
